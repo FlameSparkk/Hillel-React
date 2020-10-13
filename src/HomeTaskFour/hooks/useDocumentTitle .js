363b196ca1c92ca95c140a3bae06b9360037fe17
FlameSparkk <rutar97@gmail.com>
2020-10-14 02:11:42 +0300

    added useEffect

--- src/HomeTaskFour/hooks/useDocumentTitle .js
+++ src/HomeTaskFour/hooks/useDocumentTitle .js
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+
 export default function useDocumentTitle(title) {
-    document.title = title;
+    useEffect(() => {
+        document.title = title;
+    }, [title]);
 }
